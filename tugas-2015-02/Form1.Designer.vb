5847f90b3bf9ddd06ad562c0403ba8cb21af884a
NurmaidahWidani <nurmaidahwidani@gmail.com>
2015-10-10 19:32:12 +0700

    tugas 2
    pengumpulan tugas 2

--- tugas-2015-02/Form1.Designer.vb
+++ tugas-2015-02/Form1.Designer.vb
@@ -19,12 +19,206 @@ Partial Class Form1
 
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
-    'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
-    Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
-    End Sub
+    'Do not modify it using the code editor.<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+        <System.Diagnostics.DebuggerStepThrough()> _
+        Private Sub InitializeComponent()
+            Me.outputText = New System.Windows.Forms.TextBox()
+            Me.bt1 = New System.Windows.Forms.Button()
+            Me.bt2 = New System.Windows.Forms.Button()
+            Me.bt3 = New System.Windows.Forms.Button()
+            Me.bt4 = New System.Windows.Forms.Button()
+            Me.bt5 = New System.Windows.Forms.Button()
+            Me.bt6 = New System.Windows.Forms.Button()
+            Me.bt7 = New System.Windows.Forms.Button()
+            Me.bt8 = New System.Windows.Forms.Button()
+            Me.bt9 = New System.Windows.Forms.Button()
+            Me.bt0 = New System.Windows.Forms.Button()
+            Me.Tambah = New System.Windows.Forms.Button()
+            Me.Kurang = New System.Windows.Forms.Button()
+            Me.Hapus = New System.Windows.Forms.Button()
+            Me.Equals = New System.Windows.Forms.Button()
+            Me.SuspendLayout()
+            '
+            'outputText
+            '
+            Me.outputText.Location = New System.Drawing.Point(12, 12)
+            Me.outputText.Name = "outputText"
+            Me.outputText.ReadOnly = True
+            Me.outputText.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+            Me.outputText.Size = New System.Drawing.Size(257, 20)
+            Me.outputText.TabIndex = 0
+            Me.outputText.Text = "0"
+            '
+            'bt1
+            '
+            Me.bt1.Location = New System.Drawing.Point(12, 150)
+            Me.bt1.Name = "bt1"
+            Me.bt1.Size = New System.Drawing.Size(51, 33)
+            Me.bt1.TabIndex = 1
+            Me.bt1.Text = "1"
+            Me.bt1.UseVisualStyleBackColor = True
+            '
+            'bt2
+            '
+            Me.bt2.Location = New System.Drawing.Point(84, 150)
+            Me.bt2.Name = "bt2"
+            Me.bt2.Size = New System.Drawing.Size(50, 33)
+            Me.bt2.TabIndex = 2
+            Me.bt2.Text = "2"
+            Me.bt2.UseVisualStyleBackColor = True
+            '
+            'bt3
+            '
+            Me.bt3.Location = New System.Drawing.Point(156, 150)
+            Me.bt3.Name = "bt3"
+            Me.bt3.Size = New System.Drawing.Size(49, 33)
+            Me.bt3.TabIndex = 3
+            Me.bt3.Text = "3"
+            Me.bt3.UseVisualStyleBackColor = True
+            '
+            'bt4
+            '
+            Me.bt4.Location = New System.Drawing.Point(12, 93)
+            Me.bt4.Name = "bt4"
+            Me.bt4.Size = New System.Drawing.Size(51, 38)
+            Me.bt4.TabIndex = 4
+            Me.bt4.Text = "4"
+            Me.bt4.UseVisualStyleBackColor = True
+            '
+            'bt5
+            '
+            Me.bt5.Location = New System.Drawing.Point(84, 93)
+            Me.bt5.Name = "bt5"
+            Me.bt5.Size = New System.Drawing.Size(50, 38)
+            Me.bt5.TabIndex = 5
+            Me.bt5.Text = "5"
+            Me.bt5.UseVisualStyleBackColor = True
+            '
+            'bt6
+            '
+            Me.bt6.Location = New System.Drawing.Point(156, 93)
+            Me.bt6.Name = "bt6"
+            Me.bt6.Size = New System.Drawing.Size(49, 38)
+            Me.bt6.TabIndex = 6
+            Me.bt6.Text = "6"
+            Me.bt6.UseVisualStyleBackColor = True
+            '
+            'bt7
+            '
+            Me.bt7.Location = New System.Drawing.Point(12, 38)
+            Me.bt7.Name = "bt7"
+            Me.bt7.Size = New System.Drawing.Size(51, 38)
+            Me.bt7.TabIndex = 7
+            Me.bt7.Text = "7"
+            Me.bt7.UseVisualStyleBackColor = True
+            '
+            'bt8
+            '
+            Me.bt8.Location = New System.Drawing.Point(84, 38)
+            Me.bt8.Name = "bt8"
+            Me.bt8.Size = New System.Drawing.Size(50, 38)
+            Me.bt8.TabIndex = 8
+            Me.bt8.Text = "8"
+            Me.bt8.UseVisualStyleBackColor = True
+            '
+            'bt9
+            '
+            Me.bt9.Location = New System.Drawing.Point(156, 38)
+            Me.bt9.Name = "bt9"
+            Me.bt9.Size = New System.Drawing.Size(49, 38)
+            Me.bt9.TabIndex = 9
+            Me.bt9.Text = "9"
+            Me.bt9.UseVisualStyleBackColor = True
+            '
+            'bt0
+            '
+            Me.bt0.Location = New System.Drawing.Point(12, 205)
+            Me.bt0.Name = "bt0"
+            Me.bt0.Size = New System.Drawing.Size(51, 35)
+            Me.bt0.TabIndex = 10
+            Me.bt0.Text = "0"
+            Me.bt0.UseVisualStyleBackColor = True
+            '
+            'Tambah
+            '
+            Me.Tambah.Location = New System.Drawing.Point(222, 205)
+            Me.Tambah.Name = "Tambah"
+            Me.Tambah.Size = New System.Drawing.Size(47, 35)
+            Me.Tambah.TabIndex = 11
+            Me.Tambah.Text = "+"
+            Me.Tambah.UseVisualStyleBackColor = True
+            '
+            'Kurang
+            '
+            Me.Kurang.Location = New System.Drawing.Point(222, 150)
+            Me.Kurang.Name = "Kurang"
+            Me.Kurang.Size = New System.Drawing.Size(47, 33)
+            Me.Kurang.TabIndex = 12
+            Me.Kurang.Text = "-"
+            Me.Kurang.UseVisualStyleBackColor = True
+            '
+            'Hapus
+            '
+            Me.Hapus.Location = New System.Drawing.Point(222, 38)
+            Me.Hapus.Name = "Hapus"
+            Me.Hapus.Size = New System.Drawing.Size(47, 93)
+            Me.Hapus.TabIndex = 13
+            Me.Hapus.Text = "Clear"
+            Me.Hapus.UseVisualStyleBackColor = True
+            '
+            'Equals
+            '
+            Me.Equals.Location = New System.Drawing.Point(84, 205)
+            Me.Equals.Name = "Equals"
+            Me.Equals.Size = New System.Drawing.Size(121, 35)
+            Me.Equals.TabIndex = 14
+            Me.Equals.Text = "="
+            Me.Equals.UseVisualStyleBackColor = True
+            '
+            'Form1
+            '
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(286, 257)
+            Me.Controls.Add(Me.Equals)
+            Me.Controls.Add(Me.Hapus)
+            Me.Controls.Add(Me.Kurang)
+            Me.Controls.Add(Me.Tambah)
+            Me.Controls.Add(Me.bt0)
+            Me.Controls.Add(Me.bt9)
+            Me.Controls.Add(Me.bt8)
+            Me.Controls.Add(Me.bt7)
+            Me.Controls.Add(Me.bt6)
+            Me.Controls.Add(Me.bt5)
+            Me.Controls.Add(Me.bt4)
+            Me.Controls.Add(Me.bt3)
+            Me.Controls.Add(Me.bt2)
+            Me.Controls.Add(Me.bt1)
+            Me.Controls.Add(Me.outputText)
+            Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+            Me.MinimizeBox = False
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            Me.ResumeLayout(False)
+            Me.PerformLayout()
 
-End Class
+        End Sub
+        Friend WithEvents outputText As System.Windows.Forms.TextBox
+        Friend WithEvents bt1 As System.Windows.Forms.Button
+        Friend WithEvents bt2 As System.Windows.Forms.Button
+        Friend WithEvents bt3 As System.Windows.Forms.Button
+        Friend WithEvents bt4 As System.Windows.Forms.Button
+        Friend WithEvents bt5 As System.Windows.Forms.Button
+        Friend WithEvents bt6 As System.Windows.Forms.Button
+        Friend WithEvents bt7 As System.Windows.Forms.Button
+        Friend WithEvents bt8 As System.Windows.Forms.Button
+        Friend WithEvents bt9 As System.Windows.Forms.Button
+        Friend WithEvents bt0 As System.Windows.Forms.Button
+        Friend WithEvents Tambah As System.Windows.Forms.Button
+        Friend WithEvents Kurang As System.Windows.Forms.Button
+        Friend WithEvents Hapus As System.Windows.Forms.Button
+        Friend WithEvents Equals As System.Windows.Forms.Button
+
+    End Class
+
+ 
--- tugas-2015-02/Form1.vb
+++ tugas-2015-02/Form1.vb
@@ -1,3 +1,200 @@
 ﻿Public Class Form1
+    Dim input, lewat As Integer
+    Public Sub New()
 
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+        input = 0
+        lewat = 0
+    End Sub
+
+    Private Sub Kurang_Click(sender As Object, e As EventArgs) Handles Kurang.Click
+        input = Convert.ToInt32(outputText.Text)
+        lewat = 2
+    End Sub
+
+    Private Sub Tambah_Click(sender As Object, e As EventArgs) Handles Tambah.Click
+        input += Convert.ToInt32(outputText.Text)
+        lewat = 3
+    End Sub
+
+    Private Sub Equals_Click(sender As Object, e As EventArgs) Handles Equals.Click
+        If lewat = 2 Then
+            input -= Convert.ToInt32(outputText.Text)
+        Else
+            input = input + outputText.Text
+        End If
+        outputText.Text = input
+        lewat = 1
+        input = 0
+    End Sub
+
+    Private Sub Hapus_Click(sender As Object, e As EventArgs) Handles Hapus.Click
+        outputText.Clear()
+        outputText.Text = 0
+        lewat = 0
+    End Sub
+
+    Private Sub bt0_Click(sender As Object, e As EventArgs) Handles bt0.Click
+        If outputText.Text = 0 Then
+            outputText.Text = 0
+        Else
+            outputText.Text &= 0
+        End If
+    End Sub
+
+    Private Sub bt1_Click(sender As Object, e As EventArgs) Handles bt1.Click
+        If lewat >= 1 Then
+            outputText.Text = 0
+            If outputText.Text = 0 Then
+                outputText.Text = 1
+            Else
+                outputText.Text &= 1
+            End If
+        Else
+            If outputText.Text = 0 Then
+                outputText.Text = 1
+            Else
+                outputText.Text &= 1
+            End If
+        End If
+    End Sub
+
+    Private Sub bt2_Click(sender As Object, e As EventArgs) Handles bt2.Click
+        If lewat >= 1 Then
+            outputText.Text = 0
+            If outputText.Text = 0 Then
+                outputText.Text = 2
+            Else
+                outputText.Text &= 2
+            End If
+        Else
+            If outputText.Text = 0 Then
+                outputText.Text = 2
+            Else
+                outputText.Text &= 2
+            End If
+        End If
+    End Sub
+
+    Private Sub bt3_Click(sender As Object, e As EventArgs) Handles bt3.Click
+        If lewat >= 1 Then
+            outputText.Text = 0
+            If outputText.Text = 0 Then
+                outputText.Text = 3
+            Else
+                outputText.Text &= 3
+            End If
+        Else
+            If outputText.Text = 0 Then
+                outputText.Text = 3
+            Else
+                outputText.Text &= 3
+            End If
+        End If
+    End Sub
+
+    Private Sub bt4_Click(sender As Object, e As EventArgs) Handles bt4.Click
+        If lewat >= 1 Then
+            outputText.Text = 0
+            If outputText.Text = 0 Then
+                outputText.Text = 4
+            Else
+                outputText.Text &= 4
+            End If
+        Else
+            If outputText.Text = 0 Then
+                outputText.Text = 4
+            Else
+                outputText.Text &= 4
+            End If
+        End If
+    End Sub
+
+    Private Sub bt5_Click(sender As Object, e As EventArgs) Handles bt5.Click
+        If lewat >= 1 Then
+            outputText.Text = 0
+            If outputText.Text = 0 Then
+                outputText.Text = 5
+            Else
+                outputText.Text &= 5
+            End If
+        Else
+            If outputText.Text = 0 Then
+                outputText.Text = 5
+            Else
+                outputText.Text &= 5
+            End If
+        End If
+    End Sub
+
+    Private Sub bt6_Click(sender As Object, e As EventArgs) Handles bt6.Click
+        If lewat >= 1 Then
+            outputText.Text = 0
+            If outputText.Text = 0 Then
+                outputText.Text = 6
+            Else
+                outputText.Text &= 6
+            End If
+        Else
+            If outputText.Text = 0 Then
+                outputText.Text = 6
+            Else
+                outputText.Text &= 6
+            End If
+        End If
+    End Sub
+
+    Private Sub bt7_Click(sender As Object, e As EventArgs) Handles bt7.Click
+        If lewat >= 1 Then
+            outputText.Text = 0
+            If outputText.Text = 0 Then
+                outputText.Text = 7
+            Else
+                outputText.Text &= 7
+            End If
+        Else
+            If outputText.Text = 0 Then
+                outputText.Text = 7
+            Else
+                outputText.Text &= 7
+            End If
+        End If
+    End Sub
+
+    Private Sub bt8_Click(sender As Object, e As EventArgs) Handles bt8.Click
+        If lewat >= 1 Then
+            outputText.Text = 0
+            If outputText.Text = 0 Then
+                outputText.Text = 8
+            Else
+                outputText.Text &= 8
+            End If
+        Else
+            If outputText.Text = 0 Then
+                outputText.Text = 8
+            Else
+                outputText.Text &= 8
+            End If
+        End If
+    End Sub
+
+    Private Sub bt9_Click(sender As Object, e As EventArgs) Handles bt9.Click
+        If lewat >= 1 Then
+            outputText.Text = 0
+            If outputText.Text = 0 Then
+                outputText.Text = 9
+            Else
+                outputText.Text &= 9
+            End If
+        Else
+            If outputText.Text = 0 Then
+                outputText.Text = 9
+            Else
+                outputText.Text &= 9
+            End If
+        End If
+    End Sub
 End Class
